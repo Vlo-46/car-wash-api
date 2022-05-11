ca02c549cfd1f75dc6eb551384b9a55f99683993
Vlo-46 <volodya.galstyan98@mail.ru>
2022-05-11 14:32:04 +0400

    ATP-006

---
+++ models/technic_users.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class Technic_users extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+
+    Technic_users.init({
+        technician_id: DataTypes.INTEGER,
+        user_id: DataTypes.INTEGER
+    }, {
+        sequelize,
+        modelName: 'Technic_users',
+    });
+
+    const User = sequelize.define('User')
+    Technic_users.belongsTo(User, {
+        foreignKey: 'id'
+    })
+
+    return Technic_users;
+};
